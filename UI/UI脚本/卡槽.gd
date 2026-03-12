9631615c8785e67e168139dc9c2f14fa294c2e2a
jiabanfei1451 <2200255183@qq.com>
2026-03-12 22:27:04 +0800

    Card selection test spript

--- UI/UI脚本/卡槽.gd
+++ UI/UI脚本/卡槽.gd
@@ -1,5 +1,6 @@
 extends Button
 @export_group("卡槽状态")
+@export var 启用 : bool = true
 var 器械 : PackedScene
 @export var 器械ID : int
 @export var 消耗 : int = 50
@@ -16,39 +17,42 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$"消耗".text = str(消耗)
+	$"消耗".text = str(消耗) #消耗显示
 	器械 = 精灵图列表.Pack[器械ID-1]
 	$"图片/显示图片".texture = 精灵图列表.img[器械ID-1]
-	if get_tree().current_scene.当前器械 == 器械:
-		modulate = Color(0.735, 0.735, 0.735, 1.0)
-	else:modulate = Color(1.0, 1.0, 1.0, 1.0)
-	
-	if Input.is_action_just_released("右键") and get_tree().current_scene.当前器械 != 器械:
-		取消选定()
+	if 启用 == true:
+		if get_tree().current_scene.当前器械 == 器械:
+			modulate = Color(0.735, 0.735, 0.735, 1.0)
+		else:modulate = Color(1.0, 1.0, 1.0, 1.0)
 		
-	if Input.is_action_just_released(绑定快捷键):
-		被选中()
+		if Input.is_action_just_released("右键"):
+			取消选定()
+			
+		if Input.is_action_just_released(绑定快捷键):
+			被选中()
 
-	if get_tree().current_scene.器械能 < 消耗:
-		modulate = Color(0.5, 0.5, 0.5, 1.0)
-	else:
-		if get_tree().current_scene.当前器械 != 器械:
-			modulate = Color(1.0, 1.0, 1.0, 1.0)
-	if get_tree().current_scene.正在使用其他属性 == true:
-		选用状态 = false
-		
-	$"冷却".visible = 冷却中
+		if get_tree().current_scene.器械能 < 消耗:
+			modulate = Color(0.5, 0.5, 0.5, 1.0)
+		else:
+			if get_tree().current_scene.当前器械 != 器械:
+				modulate = Color(1.0, 1.0, 1.0, 1.0)
+		if get_tree().current_scene.正在使用其他属性 == true:
+			选用状态 = false
+			
+		$"冷却".visible = 冷却中
 
 func _on_pressed() -> void:
 	被选中()
 
 
 func _on_button_down() -> void:
-	modulate = Color(1.825, 1.825, 1.825, 1.0)
+	if 启用 == true:
+		modulate = Color(1.825, 1.825, 1.825, 1.0)
 
 
 func _on_button_up() -> void:
-	modulate = Color(1.0, 1.0, 1.0, 1.0)
+	if 启用 == true:
+		modulate = Color(1.0, 1.0, 1.0, 1.0)
 	
 func 开始冷却():
 	冷却中 = true
@@ -58,22 +62,28 @@ func 开始冷却():
 	await get_tree().create_timer(冷却).timeout
 	冷却中 = false
 func 被选中():
-	if get_tree().current_scene.器械能 >= 消耗:
-		if get_tree().current_scene.当前器械 != 器械:
-			if 冷却中 == false:
-				选定()
-		else:
-			取消选定()
+	if 启用 == true:
+		if get_tree().current_scene.器械能 >= 消耗:
+			if get_tree().current_scene.当前器械 != 器械:
+				if 冷却中 == false:
+					选定()
+			else:
+				取消选定()
 func 选定():
-	get_tree().current_scene.禁用()
-	get_tree().current_scene.来自 = $"."
-	get_tree().current_scene.当前器械 = 器械
-	$"选用".play()
+	if 启用 == true:
+		get_tree().current_scene.禁用()
+		get_tree().current_scene.来自 = $"."
+		get_tree().current_scene.当前器械 = 器械
+		$"选用".play()
 
 func 取消选定():
-	get_tree().current_scene.当前器械 = null
-	$"取消".play()
+	if 启用 == true:
+		if get_tree().current_scene.当前器械 == 器械:
+			$"取消".play()
+			get_tree().current_scene.当前器械 = null
+		
 func 已被放置():
-	get_tree().current_scene.当前器械 = null
-	get_tree().current_scene.器械能 -= 消耗
-	开始冷却()
+	if 启用 == true:
+		get_tree().current_scene.当前器械 = null
+		get_tree().current_scene.器械能 -= 消耗
+		开始冷却()
---
+++ UI/UI脚本/选卡ui.gd
@@ -0,0 +1,54 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	开始过度()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func 开始过度():
+	var x = create_tween()
+	$"选卡背景板".position.y = 700
+	$"卡槽".position.y = -109
+	$"完成选卡".position.y = 700
+	var 动画 = create_tween()
+	var d = create_tween()
+	x.tween_property($"完成选卡","position",Vector2(969,592),1).set_trans(Tween.TRANS_EXPO)
+	动画.tween_property($"选卡背景板","position",Vector2(0,110),1.0).set_trans(Tween.TRANS_EXPO)
+	d.tween_property($"卡槽","position",Vector2(0,0),1.0).set_trans(Tween.TRANS_EXPO)
+
+func 完成选卡():
+	var x = create_tween()
+	var 动画 = create_tween()
+	var d = create_tween()
+	x.tween_property($"完成选卡","position",Vector2(969,700),1).set_trans(Tween.TRANS_EXPO)
+	动画.tween_property($"选卡背景板","position",Vector2(0,700),1.0).set_trans(Tween.TRANS_EXPO)
+	d.tween_property($"卡槽","position",Vector2(0,-1000),1.0).set_trans(Tween.TRANS_EXPO)
+	await get_tree().create_timer(1).timeout
+	get_tree().current_scene.get_node("选卡动画").play("选卡完成")
+	await get_tree().create_timer(1.5).timeout
+	$"准备开始".scale = Vector2(0.35,0.35)
+	$"准备开始".text = "好"
+	音效()
+	await get_tree().create_timer(0.5).timeout
+	$"准备开始".scale = Vector2(0.3,0.3)
+	$"准备开始".text = "准备"
+	var xc = create_tween()
+	xc.tween_property($"准备开始","scale",Vector2(0.45,0.45),0.5).set_trans(Tween.TRANS_SINE)
+	await get_tree().create_timer(0.5).timeout
+	$"准备开始".text = "安放器械！"
+	$"准备开始".scale = Vector2(0.6,0.6)
+	await get_tree().create_timer(2).timeout
+	get_tree().current_scene.get_node("音效/音乐").音乐选项 = 0
+	get_tree().current_scene.生成节点(preload("res://UI/关卡UI.tscn"),$"..")
+	get_tree().current_scene.生成节点(preload("res://UI/信息显示.tscn"),$"..")
+	$".".queue_free()
+
+func 音效():
+	$"准备开始/播放音效".play()
+func _on_完成选卡_pressed() -> void:
+	pass # Replace with function body.
