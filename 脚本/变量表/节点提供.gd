b07eb4d8f5359014a5ec687a2bf615431b84926c
jiabanfei1451 <2200255183@qq.com>
2026-03-12 22:29:54 +0800

    Add files via upload

--- 脚本/变量表/节点提供.gd
+++ 脚本/变量表/节点提供.gd
@@ -8,3 +8,27 @@ static var 光源 : Node2D
 static var 特效 : Node2D
 static var 粒子 : Node2D
 static var 粒子2 : Node2D
+
+func 生成节点(节点:PackedScene,生成节点:String = "pass",层级:int = 0,自动排序:String = "pass"):
+	var 实例化 = 节点.instantiate()
+	实例化.z_index = 层级
+	if 自动排序 == "pass":
+		实例化.z_as_relative = false
+		实例化.y_sort_enabled = false
+	elif 自动排序 == "x":
+		实例化.z_as_relative = true
+		实例化.y_sort_enabled = false
+	elif 自动排序 == "y":
+		实例化.z_as_relative = false
+		实例化.y_sort_enabled = true
+	elif 自动排序 == "xy":
+		实例化.y_sort_enabled = true
+		实例化.z_as_relative = true
+	elif 自动排序 == "yx":
+		实例化.y_sort_enabled = true
+		实例化.z_as_relative = true
+	if 生成节点 == "pass":
+		get_tree().current_scene.get_node(生成节点).add_child(实例化)
+	else:
+		get_tree().current_scene.add_child(实例化)
+	return 实例化
